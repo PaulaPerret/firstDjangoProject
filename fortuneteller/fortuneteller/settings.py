5dfdc71e59e606894ee372406ec995222fa9bf87
PaulaPerret <83120308+PaulaPerret@users.noreply.github.com>
2021-08-15 20:39:28 -0300

    Update settings.py

--- fortuneteller/fortuneteller/settings.py
+++ fortuneteller/fortuneteller/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4k61w@+_nm7^n7b$0dg6yn$4on*@-d40_at*zwbo_*n6)15t$o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
